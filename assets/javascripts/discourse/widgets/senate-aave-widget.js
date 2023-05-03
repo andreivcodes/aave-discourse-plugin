7ffd9c083e28b7c207e920dc1934ce4a6cf65840
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-05-03 14:49:06 +0300

    wip

---
+++ assets/javascripts/discourse/widgets/senate-aave-widget.js
@@ -0,0 +1,78 @@
+import { createWidget } from "discourse/widgets/widget";
+import { h } from "virtual-dom";
+import { iconNode } from "discourse-common/lib/icon-library";
+
+export default createWidget("senate-aave", {
+  tagName: "div.senate-aave",
+
+  defaultState() {
+    return { tooltipVisible: false };
+  },
+
+  buildKey: () => "senate-aave",
+
+  html(attrs, state) {
+    let icon = iconNode("envelope");
+
+    return h(
+      "div.senate-aave-container",
+      {
+        style: {
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+      },
+      [
+        h(
+          "button.senate-aave-btn",
+          {
+            onclick: () => this.toggleTooltip(),
+            style: {
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "34px",
+              width: "249px",
+              color: "#00000",
+              background: "#333333",
+              border: "1px solid #333333",
+              cursor: "pointer",
+              padding: "0.5em 1em",
+              margin: "1em",
+            },
+          },
+          h(
+            "div",
+            { style: { display: "flex", alignItems: "center", gap: "0.5em" } },
+            [icon, `Setup Proposal Notifications`]
+          )
+        ),
+        state.tooltipVisible
+          ? h(
+              "div.tooltip",
+              {
+                style: {
+                  position: "absolute",
+                  background: "#f9f9f9",
+                  border: "1px solid #ccc",
+                  padding: "5px 10px",
+                  borderRadius: "4px",
+                  fontSize: "14px",
+                  color: "#333",
+                  whiteSpace: "nowrap",
+                  marginTop: "65px",
+                },
+              },
+              "This is a test tooltip"
+            )
+          : null,
+      ]
+    );
+  },
+
+  toggleTooltip() {
+    this.state.tooltipVisible = !this.state.tooltipVisible;
+    this.scheduleRerender();
+  },
+});
